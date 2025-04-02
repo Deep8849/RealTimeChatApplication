ee4aaa6418b7154dba481d2222c125e3eea175f1
Deep8849 <dipeshogale@gmail.com>
2025-04-02 14:24:26 +0530

    Chat Application implementation

---
+++ src/main/java/com/dip/chatapplication/chat/MessageType.java
@@ -0,0 +1,12 @@
+package com.dip.chatapplication.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/dip/chatapplication/config/WebSocketEventListener.java
@@ -0,0 +1,50 @@
+package com.dip.chatapplication.config;
+
+import com.dip.chatapplication.chat.ChatMessage;
+import com.dip.chatapplication.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+
+@Component
+//@RequiredArgsConstructor
+//@Slf4j
+
+// to inform users that a user has left the chat
+public class WebSocketEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = headerAccessor.getSessionAttributes().get("username").toString(); // because its an object so .toString
+
+        if (username != null) {
+            logger.info("User disconnected:{}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messagingTemplate.convertAndSend("/topic/public", chatMessage); // informing other users
+        }
+
+    }
+
+}
